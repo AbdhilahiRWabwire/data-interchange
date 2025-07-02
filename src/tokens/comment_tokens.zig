397b2d781535b51f8f23d4c5086e02eb8a009e5e
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-07-02 05:07:17 -0600

    Refactor Constants

--- src/tokens/comment_tokens.zig
+++ src/tokens/comment_tokens.zig
@@ -4,34 +4,34 @@ const standard: type = @import("std");
 pub const CommentToken: type = []const u8;
 
 // Comment Tokens
-pub const BLOCK_COMMENT_TOKEN: [2]CommentToken = "/*";
-pub const LINE_COMMENT_TOKEN: [2]CommentToken = "//";
-pub const INNER_LINE_COMMENT_TOKEN: [3]CommentToken = "//!";
-pub const INNER_BLOCK_COMMENT_TOKEN: [3]CommentToken = "/*!";
-pub const OUTER_LINE_COMMENT_TOKEN: [3]CommentToken = "///";
-pub const OUTER_BLOCK_COMMENT_TOKEN: [3]CommentToken = "/**";
-pub const OCTOTHORPE_BLOCK_COMMENT_TOKEN: [2]CommentToken = "#*";
-pub const OCTOTHORPE_LINE_COMMENT_TOKEN: [1]CommentToken = "#";
-pub const OCTOTHROPE_INNER_LINE_COMMENT_TOKEN: [2]CommentToken = "#!";
-pub const OCTOTHORPE_INNER_BLOCK_COMMENT_TOKEN: [3]CommentToken = "#*!";
-pub const OCTOTHROPE_OUTER_LINE_COMMENT_TOKEN: [3]CommentToken = "###";
-pub const OCTOTHORPE_OUTER_BLOCK_COMMENT_TOKEN: [3]CommentToken = "#**";
+pub const BLOCK_TOKEN: [2]CommentToken = "/*";
+pub const LINE_TOKEN: [2]CommentToken = "//";
+pub const INNER_LINE_TOKEN: [3]CommentToken = "//!";
+pub const INNER_BLOCK_TOKEN: [3]CommentToken = "/*!";
+pub const OUTER_LINE_TOKEN: [3]CommentToken = "///";
+pub const OUTER_BLOCK_TOKEN: [3]CommentToken = "/**";
+pub const OCTOTHORPE_BLOCK_TOKEN: [2]CommentToken = "#*";
+pub const OCTOTHORPE_LINE_TOKEN: [1]CommentToken = "#";
+pub const OCTOTHROPE_INNER_LINE_TOKEN: [2]CommentToken = "#!";
+pub const OCTOTHORPE_INNER_BLOCK_TOKEN: [3]CommentToken = "#*!";
+pub const OCTOTHROPE_OUTER_LINE_TOKEN: [3]CommentToken = "###";
+pub const OCTOTHORPE_OUTER_BLOCK_TOKEN: [3]CommentToken = "#**";
 
 // Comment Token Vector
 pub fn comments_vector() @Vector(12, CommentToken) {
     const comments: @Vector(12, CommentToken) = @Vector(12, CommentToken){
-        BLOCK_COMMENT_TOKEN,
-        LINE_COMMENT_TOKEN,
-        INNER_LINE_COMMENT_TOKEN,
-        INNER_BLOCK_COMMENT_TOKEN,
-        OUTER_LINE_COMMENT_TOKEN,
-        OUTER_BLOCK_COMMENT_TOKEN,
-        OCTOTHORPE_BLOCK_COMMENT_TOKEN,
-        OCTOTHORPE_LINE_COMMENT_TOKEN,
-        OCTOTHROPE_INNER_LINE_COMMENT_TOKEN,
-        OCTOTHORPE_INNER_BLOCK_COMMENT_TOKEN,
-        OCTOTHROPE_OUTER_LINE_COMMENT_TOKEN,
-        OCTOTHORPE_OUTER_BLOCK_COMMENT_TOKEN,
+        BLOCK_TOKEN,
+        LINE_TOKEN,
+        INNER_LINE_TOKEN,
+        INNER_BLOCK_TOKEN,
+        OUTER_LINE_TOKEN,
+        OUTER_BLOCK_TOKEN,
+        OCTOTHORPE_BLOCK_TOKEN,
+        OCTOTHORPE_LINE_TOKEN,
+        OCTOTHROPE_INNER_LINE_TOKEN,
+        OCTOTHORPE_INNER_BLOCK_TOKEN,
+        OCTOTHROPE_OUTER_LINE_TOKEN,
+        OCTOTHORPE_OUTER_BLOCK_TOKEN,
     };
 
     return comments;
