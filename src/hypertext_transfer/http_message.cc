bd91ac4cc4fbbea12083a3070c908df18cacab3d
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-07-21 00:19:59 -0600

    Fix Generics

--- src/hypertext_transfer/http_message.cc
+++ src/hypertext_transfer/http_message.cc
@@ -1,45 +1,18 @@
-#include <vector>
-#include "./http_headers.hh"
 #include "./http_message.hh"
-#include "./http_methods.hh"
-#include "./http_security_directives.hh"
-#include "./http_status_codes.hh"
-#include "./http_versions.hh"
 
 namespace hypertext_transfer {
     using namespace std;
     using namespace hypertext_transfer;
-
-    // Hypertext Transfer Protocol Request Definition
-    struct HttpRequest {
-        HttpBody body;
-        vector<HttpHeader> headers;
-        HttpMethod method;
-        string path;
-        vector<HttpSecurityDirective> security_directives;
-        HttpStatusCode status_code;
-        HttpStatusText status_text;
-        HttpVersion version;
-    };
-
-    // Hypertext Transfer Protocol Response Definition
-    struct HttpResponse {
-        HttpBody body;
-        vector<HttpHeader> headers;
-        HttpMethod method;
-        vector<HttpSecurityDirective> security_directives;
-        HttpStatusCode status_code;
-        HttpStatusText status_text;
-        HttpVersion version;
-    };
-
-    // Hypertext Transfer Protocol Request Message
-    HttpRequest request_message(HttpRequest request) {
+    
+    // Hypertext Transfer Protocol Request Message    
+    template <typename HttpBody>
+    HttpRequest<HttpBody> request_message(HttpRequest<HttpBody> request) {
         return request;
     }
 
     // Hypertext Transfer Protocol Response Message
-    HttpResponse reponse_message(HttpResponse response) {
+    template <typename HttpBody>
+    HttpResponse<HttpBody> reponse_message(HttpResponse<HttpBody> response) {
         return response;
     }
 }
--- src/hypertext_transfer/http_message.hh
+++ src/hypertext_transfer/http_message.hh
@@ -2,25 +2,55 @@
 #define HTTP_MESSAGE_HH
 
 #include <string>
+#include <vector>
+#include "./http_headers.hh"
+#include "./http_methods.hh"
+#include "./http_security_directives.hh"
+#include "./http_status_codes.hh"
+#include "./http_versions.hh"
+
 
 namespace hypertext_transfer {
     using namespace std;
     using namespace hypertext_transfer;
 
-    // Hypertext Transfer Protocol Body Definition
-    typedef string HttpBody;
-
+    
+    
     // Hypertext Transfer Protocol Request Definition
-    struct HttpRequest;
+    // Hypertext Transfer Protocol Body Generic Type Declaration
+    template <typename HttpBody>
+    struct HttpRequest {
+        HttpBody body;
+        vector<HttpHeader> headers;
+        HttpMethod method;
+        string path;
+        vector<HttpSecurityDirective> security_directives;
+        HttpStatusCode status_code;
+        HttpStatusText status_text;
+        HttpVersion version;
+    };
 
     // Hypertext Transfer Protocol Response Definition
-    struct HttpResponse;
+    // Hypertext Transfer Protocol Body Generic Type Declaration
+    template <typename HttpBody>
+    struct HttpResponse {
+        HttpBody body;
+        vector<HttpHeader> headers;
+        HttpMethod method;
+        vector<HttpSecurityDirective> security_directives;
+        HttpStatusCode status_code;
+        HttpStatusText status_text;
+        HttpVersion version;
+    };
 
-    // Hypertext Transfer Protocol Request Message
-    HttpRequest request_message(HttpRequest request);
-
-    // Hypertext Transfer Protocol Response Message
-    HttpResponse reponse_message(HttpResponse response);
+    
+    // Hypertext Transfer Protocol Request Message    
+    template <typename HttpBody>
+    HttpRequest<HttpBody> request_message(HttpRequest<HttpBody> request);
+    
+    // Hypertext Transfer Protocol Response Message    
+    template <typename HttpBody>
+    HttpResponse<HttpBody> reponse_message(HttpResponse<HttpBody> response);
 }
 
 
